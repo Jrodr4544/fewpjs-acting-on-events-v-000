f285a4d77b7b886042f37ccba2822a33422742fd
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-23 02:23:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function moveDodgerLeft(){
 }
   
 function moveDodgerRight(){
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`;
+  if (right > 0) {
+    dodger.style.right = `${left + 1}px`;
   }
 }  
 
