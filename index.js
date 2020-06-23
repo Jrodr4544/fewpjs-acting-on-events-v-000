1d6afcb84aa0ebf9ce0d38b6413a7c4df9982290
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-23 02:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ let dodger = document.getElementById("dodger");
     dodger.style.backgroundColor = "#FF69B4";
     
 function moveDodgerLeft(){
-  
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
   
 function moveDodgerRight(){
